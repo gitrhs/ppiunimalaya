d28098780aaf782c10a9ffe70026e376728cc0a2
gitrhs <48622808+gitrhs@users.noreply.github.com>
2023-07-25 13:14:25 +0700

    update main page

--- index.php
+++ index.php
@@ -5,127 +5,6 @@
     //header
     include 'core/header.html';
     ?>
-    <style>
-        .section1{
-            color: white;
-        }
-        .title{
-            font-size: 50px;
-        }
-        .aboutbutton{
-            font-size: 20px;
-        }
-        .btn .rounded-pill{
-            border: 20px !important;
-        }
-        .btn-sosmed {
-            
-        }
-        #instagram-div{
-            display: none;
-        }
-        #facebook-div{
-            display: none;
-        }
-        #youtube-div{
-            display: none;
-        }
-        #spotify-div{
-            display: none;
-        }
-        .horizontal-line {
-          display: inline-block;
-          width: 420px;
-          height: 2px;
-          background-color: black;
-          margin-bottom: 30px;
-          margin-top: 20px;
-        }
-        .spotify-text{
-            
-        }
-        .btn-spotify{
-            background-color: #61ce70;
-            color: white;
-            margin-top: 20px;
-        }
-        .btn-facebook{
-            background-color: #2374e1;
-            color: white;
-            margin-top: 20px;
-        }
-        .btn-youtube{
-            background-color: #CD201F;
-            color: white;
-            margin-top: 20px;
-        }
-        .btn-instagram{
-            margin-top: 20px;
-            border: 1px solid #e4405f;
-            color: #e4405f;
-            background-color: transparent;
-        }
-        .btn-instagram:hover {
-          background-color: #e4405f;
-          color: #fff;
-        }
-        .instagram-feed {
-          max-width: 500px;
-          margin-top: 10px;
-          margin-bottom: 10px;
-        }
-        .youtube-background{
-            width: 100%;
-            background-color: #fc5757;
-            border-radius: 10px;
-        }
-        .instagram-color{
-            background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
-            -webkit-background-clip: text;
-            background-clip: text;
-            color: transparent;
-            -webkit-text-fill-color: transparent;
-        }
-        .elementor-shape-fill {
-          fill: #6381bc;
-        }
-        .elementor-shape-fill-white {
-          fill: white;
-        }
-    
-        .elementor-shape-fill-blue {
-          fill: #6381bb;
-        }
-        @media only screen and (max-width: 600px) {
-          .title {
-            font-size: 35px;
-          }
-          .btn-sosmed {
-            padding: 0.25rem 0.5rem;
-            font-size: 0.875rem;
-            line-height: 1.5;
-            border-radius: 0.2rem;
-          }
-          .btn-spotify{
-            margin-top: 0px;
-          }
-          .btn-facebook{
-            margin-top: 0px;
-          }
-          .btn-youtube{
-              margin-top: 0px;
-          }
-          .spotify-text{
-            text-align: center;
-          }
-          .horizontal-line {
-          display: inline-block;
-          width: 250px;
-          height: 2px;
-          background-color: black;
-        }
-        }
-    </style>
     </head>
     <body>
         <?php
@@ -157,7 +36,7 @@
         </div>
         <!----->
         <!-- Section 2 - Social Media -->
-        <div style="background-color: white; margin-top: -5px;">
+        <div style="background-color: white; margin-top: -5px; color: #101f32;">
             <!-- Wave -->
             <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 283.5 27.8" preserveaspectratio="none">
               <path class="elementor-shape-fill" d="M283.5,9.7c0,0-7.3,4.3-14,4.6c-6.8,0.3-12.6,0-20.9-1.5c-11.3-2-33.1-10.1-44.7-5.7	s-12.1,4.6-18,7.4c-6.6,3.2-20,9.6-36.6,9.3C131.6,23.5,99.5,7.2,86.3,8c-1.4,0.1-6.6,0.8-10.5,2c-3.8,1.2-9.4,3.8-17,4.7	c-3.2,0.4-8.3,1.1-14.2,0.9c-1.5-0.1-6.3-0.4-12-1.6c-5.7-1.2-11-3.1-15.8-3.7C6.5,9.2,0,10.8,0,10.8V0h283.5V9.7z M260.8,11.3	c-0.7-1-2-0.4-4.3-0.4c-2.3,0-6.1-1.2-5.8-1.1c0.3,0.1,3.1,1.5,6,1.9C259.7,12.2,261.4,12.3,260.8,11.3z M242.4,8.6	c0,0-2.4-0.2-5.6-0.9c-3.2-0.8-10.3-2.8-15.1-3.5c-8.2-1.1-15.8,0-15.1,0.1c0.8,0.1,9.6-0.6,17.6,1.1c3.3,0.7,9.3,2.2,12.4,2.7	C239.9,8.7,242.4,8.6,242.4,8.6z M185.2,8.5c1.7-0.7-13.3,4.7-18.5,6.1c-2.1,0.6-6.2,1.6-10,2c-3.9,0.4-8.9,0.4-8.8,0.5	c0,0.2,5.8,0.8,11.2,0c5.4-0.8,5.2-1.1,7.6-1.6C170.5,14.7,183.5,9.2,185.2,8.5z M199.1,6.9c0.2,0-0.8-0.4-4.8,1.1	c-4,1.5-6.7,3.5-6.9,3.7c-0.2,0.1,3.5-1.8,6.6-3C197,7.5,199,6.9,199.1,6.9z M283,6c-0.1,0.1-1.9,1.1-4.8,2.5s-6.9,2.8-6.7,2.7	c0.2,0,3.5-0.6,7.4-2.5C282.8,6.8,283.1,5.9,283,6z M31.3,11.6c0.1-0.2-1.9-0.2-4.5-1.2s-5.4-1.6-7.8-2C15,7.6,7.3,8.5,7.7,8.6	C8,8.7,15.9,8.3,20.2,9.3c2.2,0.5,2.4,0.5,5.7,1.6S31.2,11.9,31.3,11.6z M73,9.2c0.4-0.1,3.5-1.6,8.4-2.6c4.9-1.1,8.9-0.5,8.9-0.8	c0-0.3-1-0.9-6.2-0.3S72.6,9.3,73,9.2z M71.6,6.7C71.8,6.8,75,5.4,77.3,5c2.3-0.3,1.9-0.5,1.9-0.6c0-0.1-1.1-0.2-2.7,0.2	C74.8,5.1,71.4,6.6,71.6,6.7z M93.6,4.4c0.1,0.2,3.5,0.8,5.6,1.8c2.1,1,1.8,0.6,1.9,0.5c0.1-0.1-0.8-0.8-2.4-1.3	C97.1,4.8,93.5,4.2,93.6,4.4z M65.4,11.1c-0.1,0.3,0.3,0.5,1.9-0.2s2.6-1.3,2.2-1.2s-0.9,0.4-2.5,0.8C65.3,10.9,65.5,10.8,65.4,11.1	z M34.5,12.4c-0.2,0,2.1,0.8,3.3,0.9c1.2,0.1,2,0.1,2-0.2c0-0.3-0.1-0.5-1.6-0.4C36.6,12.8,34.7,12.4,34.5,12.4z M152.2,21.1	c-0.1,0.1-2.4-0.3-7.5-0.3c-5,0-13.6-2.4-17.2-3.5c-3.6-1.1,10,3.9,16.5,4.1C150.5,21.6,152.3,21,152.2,21.1z" />
@@ -239,21 +118,20 @@
                     <div class="row">
                         <div class="col-12 col-lg-6 text-center">
                             <div class="youtube-background">
-                                <iframe width=96% style="aspect-ratio: 16/9; margin-top: 15px; border-radius: 10px;" src="https://www.youtube.com/embed/-8AkpaFBYJM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                                <div style="width=96%;">
-                                    <div class="row">
-                                        <div class="col-3">
-                                            1
-                                        </div>
-                                        <div class="col-3">
-                                            2
-                                        </div>
-                                        <div class="col-3">
-                                            3
-                                        </div>
-                                        <div class="col-3">
-                                            4
-                                        </div>
+                                <iframe id="ytVideo" width=96% style="aspect-ratio: 16/9; margin-top: 15px; border-radius: 10px;" src="https://www.youtube.com/embed/-8AkpaFBYJM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                
+                                <div class="d-flex justify-content-center">
+                                    <div class="p-1">
+                                        <img src="https://i.ytimg.com/vi/-8AkpaFBYJM/hqdefault.jpg" width=90% onclick="ytplayer(this)" value="-8AkpaFBYJM" class="ytplayer active">
+                                    </div>
+                                    <div class="p-1">
+                                        <img src="https://i.ytimg.com/vi/PLIh1biG-Hs/hqdefault.jpg" width=90% onclick="ytplayer((this))" value="PLIh1biG-Hs" class="ytplayer">
+                                    </div>
+                                    <div class="p-1">
+                                        <img src="https://i.ytimg.com/vi/xlrPBezQDY8/hqdefault.jpg" width=90% onclick="ytplayer((this))" value="xlrPBezQDY8" class="ytplayer">
+                                    </div>
+                                    <div class="p-1">
+                                        <img src="https://i.ytimg.com/vi/TgRZqmlTxdc/hqdefault.jpg" width=90% onclick="ytplayer((this))" value="TgRZqmlTxdc" class="ytplayer">
                                     </div>
                                 </div>
                             </div>
@@ -287,12 +165,16 @@
                 </div>
             </div>
              <!-- Wave -->
+             <svg id="visual" viewBox="0 0 1000 100"style="width: 100%;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><rect x="0" y="0" width="1000" height="100" fill="#ffffff"></rect><path d="M0 67L5.3 68.2C10.7 69.3 21.3 71.7 32.2 70.2C43 68.7 54 63.3 64.8 61.5C75.7 59.7 86.3 61.3 97 62.7C107.7 64 118.3 65 129 67.5C139.7 70 150.3 74 161.2 74C172 74 183 70 193.8 68.8C204.7 67.7 215.3 69.3 226 69.7C236.7 70 247.3 69 258 67C268.7 65 279.3 62 290.2 61.8C301 61.7 312 64.3 322.8 65.2C333.7 66 344.3 65 355 66.3C365.7 67.7 376.3 71.3 387 74.2C397.7 77 408.3 79 419.2 79.8C430 80.7 441 80.3 451.8 80.2C462.7 80 473.3 80 484 78.3C494.7 76.7 505.3 73.3 516 69.7C526.7 66 537.3 62 548.2 60.5C559 59 570 60 580.8 62.3C591.7 64.7 602.3 68.3 613 69C623.7 69.7 634.3 67.3 645 65.5C655.7 63.7 666.3 62.3 677.2 63.2C688 64 699 67 709.8 67.3C720.7 67.7 731.3 65.3 742 63.5C752.7 61.7 763.3 60.3 774 60C784.7 59.7 795.3 60.3 806.2 60.3C817 60.3 828 59.7 838.8 62.5C849.7 65.3 860.3 71.7 871 71.8C881.7 72 892.3 66 903 65.8C913.7 65.7 924.3 71.3 935.2 73.2C946 75 957 73 967.8 73C978.7 73 989.3 75 994.7 76L1000 77L1000 101L994.7 101C989.3 101 978.7 101 967.8 101C957 101 946 101 935.2 101C924.3 101 913.7 101 903 101C892.3 101 881.7 101 871 101C860.3 101 849.7 101 838.8 101C828 101 817 101 806.2 101C795.3 101 784.7 101 774 101C763.3 101 752.7 101 742 101C731.3 101 720.7 101 709.8 101C699 101 688 101 677.2 101C666.3 101 655.7 101 645 101C634.3 101 623.7 101 613 101C602.3 101 591.7 101 580.8 101C570 101 559 101 548.2 101C537.3 101 526.7 101 516 101C505.3 101 494.7 101 484 101C473.3 101 462.7 101 451.8 101C441 101 430 101 419.2 101C408.3 101 397.7 101 387 101C376.3 101 365.7 101 355 101C344.3 101 333.7 101 322.8 101C312 101 301 101 290.2 101C279.3 101 268.7 101 258 101C247.3 101 236.7 101 226 101C215.3 101 204.7 101 193.8 101C183 101 172 101 161.2 101C150.3 101 139.7 101 129 101C118.3 101 107.7 101 97 101C86.3 101 75.7 101 64.8 101C54 101 43 101 32.2 101C21.3 101 10.7 101 5.3 101L0 101Z" fill="#6381bc" stroke-linecap="round" stroke-linejoin="miter"></path></svg>
         </div>
         <!-- Section 3 - About Us -->
         <div class="container-fluid">
             
         </div>
+        <!-- Transition to footer -->
+        <div class="transition-footer-div"></div>
         <!-- JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <!-- JS for social media toogle -->
         <script>
           function updatePage() {
@@ -322,6 +204,18 @@
                 // Handle the case when the value is not one of the specified options
                 break;
             }
+          }
+          function ytplayer(element) {
+            // Remove 'active' class from all images with the class 'ytplayer'
+            $('.ytplayer').removeClass('active');
+          
+            // Add 'active' class to the clicked image
+            $(element).addClass('active');
+            
+            var videoId = $(element).attr('value');
+            
+            // Update the iframe src attribute with the new video ID
+            $('#ytVideo').attr('src', 'https://www.youtube.com/embed/' + videoId);
           }
         </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
